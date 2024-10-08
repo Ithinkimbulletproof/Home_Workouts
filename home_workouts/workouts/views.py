6945272e39af53468e5eb3ee6624350499bb49b5
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-10-08 10:10:07 +0500

    Use black

--- home_workouts/workouts/views.py
+++ home_workouts/workouts/views.py
@@ -7,8 +7,10 @@ from .forms import UserRegistrationForm, UserProfileForm, UserProgressForm
 from .models import WorkoutPlan, UserProgress
 from django.contrib.auth.models import User
 
+
 def home(request):
     return render(request, "workouts/home.html")
+
 
 def register(request):
     if request.method == "POST":
@@ -16,7 +18,7 @@ def register(request):
         if form.is_valid():
             # Создание пользователя
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])  # Установка пароля
+            user.set_password(form.cleaned_data["password"])  # Установка пароля
             user.save()  # Сохранение пользователя
             login(request, user)  # Вход пользователя
             messages.success(request, "Вы успешно зарегистрированы!")
@@ -25,9 +27,11 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, "workouts/register.html", {"form": form})
 
+
 def logout_view(request):
     logout(request)
     return redirect("home")
+
 
 @login_required
 def profile(request):
@@ -41,6 +45,7 @@ def profile(request):
         form = UserProfileForm(instance=request.user.userprofile)
     return render(request, "workouts/profile.html", {"form": form})
 
+
 @login_required
 def workout_plan(request):
     user_profile = request.user.userprofile
@@ -51,15 +56,18 @@ def workout_plan(request):
 
     return render(request, "workouts/workout_plan.html", {"workouts": workouts})
 
+
 @login_required
 def workout_list(request):
     workouts = WorkoutPlan.objects.all()
     return render(request, "workouts/workout_list.html", {"workouts": workouts})
 
+
 @login_required
 def workout_detail(request, id):
     workout = WorkoutPlan.objects.get(id=id)
     return render(request, "workouts/workout_detail.html", {"workout": workout})
+
 
 class UserProgressView(TemplateView):
     template_name = "workouts/progress.html"
